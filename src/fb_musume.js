d890c198813be5be80fd7e859602b4d6d8cf95ed
wmin0 <wmin0@hotmail.com>
2015-03-09 22:12:53 +0800

    refector 2

--- src/fb_musume.js
+++ src/fb_musume.js
@@ -18,57 +18,23 @@ Character.prototype.template = [
 Character.prototype.createElement = function() {
 	var div = document.createElement('div');
 	div.innerHTML = this.template;
-	div.querySelector('.character').src = this.config.path;
+	var character = div.querySelector('.character');
+	character.src = this.config.path;
+	character.addEventListener('click', function(event) {
+		this.say(new Date());
+	}.bind(this));
 	return div;
 }
-/*
-var Character = {
-	Init: function Init(path) {
-		if (this.initialized_) {
-			return;
-		}
-		this.initialized_ = true;
-		that = this;
 
-		this.DOM_top_ = document.createElement("div");
-		this.DOM_top_.className = "fbm-top";
+Character.prototype.say = function(word) {
+	var msgBox = this.elem.querySelector('.msg-box');
+	msgBox.classList.remove('show');
+	// force to reset classList
+	msgBox.offsetHeight = msgBox.offsetHeight;
+	msgBox.innerText = word;
+	msgBox.classList.add('show');
+}
 
-		bottom_container = document.createElement("div");
-		bottom_container.className = "to-bottom";
-
-		this.DOM_msg_ = document.createElement("div");
-
-		this.DOM_character_ = document.createElement("img");
-		this.DOM_character_.className = "character";
-		this.DOM_character_.setAttribute("src", path);
-		this.DOM_character_.addEventListener("click", function(){
-			console.log("!!!!!!!!!!!!!!!");
-			that.Say(new Date());
-		});
-
-		this.DOM_top_.appendChild(this.DOM_character_);
-		bottom_container.appendChild(this.DOM_msg_);
-		this.DOM_top_.appendChild(bottom_container);
-		document.querySelector("body").appendChild(this.DOM_top_);
-
-		this.Say("Hello");
-	},
-
-	Say: function Say(msg) {
-		if (!this.initialized_) {
-			return;
-		}
-
-		var that = this;
-		that.DOM_msg_.className = "";
-		setTimeout(function(){that.DOM_msg_.className = "msg-box";},   1);
-		setTimeout(function(){that.DOM_msg_.innerText = msg;},       500);
-	},
-
-};
-
-Character.Init(chrome.extension.getURL("assets/Atago/pannpakapann.jpg"));
-*/
 var character = new Character({
 	path: chrome.extension.getURL("assets/Atago/pannpakapann.jpg")
 });
