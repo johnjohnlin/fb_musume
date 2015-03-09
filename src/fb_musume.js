10388f09416b4e45ef649be9fd57df79e490446e
johnjohnlin <johnjohnlin@gmail.com>
2015-03-09 21:16:00 +0800

    Add message box
    How to add onclick function?

--- src/fb_musume.js
+++ src/fb_musume.js
@@ -1,13 +1,46 @@
 var Character = {
 	Init: function Init(path) {
-		  this.div = document.createElement("div");
-		  this.img = document.createElement("img");
-		  this.div.appendChild(this.img);
-		  document.body.appendChild(this.div);
+		if (this.initialized_) {
+			return;
+		}
+		this.initialized_ = true;
+		that = this;
 
-		  this.div.className = "fbm-character";
-		  this.img.setAttribute("src", path);
-	}
+		this.DOM_top_ = document.createElement("div");
+		this.DOM_top_.className = "fbm-top";
+
+		bottom_container = document.createElement("div");
+		bottom_container.className = "to-bottom";
+
+		this.DOM_msg_ = document.createElement("div");
+
+		this.DOM_character_ = document.createElement("img");
+		this.DOM_character_.className = "character";
+		this.DOM_character_.setAttribute("src", path);
+		this.DOM_character_.addEventListener("click", function(){
+			console.log("!!!!!!!!!!!!!!!");
+			that.Say(new Date());
+		});
+
+		this.DOM_top_.appendChild(this.DOM_character_);
+		bottom_container.appendChild(this.DOM_msg_);
+		this.DOM_top_.appendChild(bottom_container);
+		document.querySelector("body").appendChild(this.DOM_top_);
+
+		this.Say("Hello");
+	},
+
+	Say: function Say(msg) {
+		if (!this.initialized_) {
+			return;
+		}
+
+		var that = this;
+		that.DOM_msg_.className = "";
+		setTimeout(function(){that.DOM_msg_.className = "msg-box";},   1);
+		setTimeout(function(){that.DOM_msg_.innerText = msg;},       500);
+	},
+
 };
 
 Character.Init(chrome.extension.getURL("assets/Atago/pannpakapann.jpg"));
