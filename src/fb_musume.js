0abbb83ea9ee0051e5ebaf69ef7c2711a5a281c2
johnjohnlin <johnjohnlin@gmail.com>
2015-03-10 13:00:24 +0800

    Add idle messages
    TODO:
    z-index
    Images
    Click event
    Hour event

--- src/fb_musume.js
+++ src/fb_musume.js
@@ -1,7 +1,9 @@
 var Character = function(config) {
-	this.config = config;
+	this.parseConfig(config);
 	this.elem = this.createElement();
 	document.querySelector("body").appendChild(this.elem);
+	this.hourUpdate();
+	this.onIdle();
 }
 
 Character.prototype.template = [
@@ -13,14 +15,23 @@ Character.prototype.template = [
 	'</div>'
 ].join("");
 
+Character.prototype.parseConfig = function parseConfig(config) {
+	this.config = config;
+	for (var state_name in this.config.animations) {
+		this.config.animations[state_name].forEach(function(frame) {
+			frame.path = chrome.extension.getURL("assets/" + frame.path);
+		});
+	}
+}
+
 Character.prototype.createElement = function() {
 	var div = document.createElement('div');
 	div.innerHTML = this.template;
 	var character = div.querySelector('.character');
-	character.src = this.config.path;
-	character.addEventListener('click', function(event) {
-		this.say(new Date());
-	}.bind(this));
+	character.src = this.config.animations.idle[0].path;
+	// character.addEventListener('click', function(event) {
+	// 	this.say(new Date());
+	// }.bind(this));
 	return div;
 }
 
@@ -33,6 +44,89 @@ Character.prototype.say = function(word) {
 	msgBox.classList.add('show');
 }
 
+Character.prototype.changeAnimation = function() {
+}
+
+Character.prototype.onIdle = function() {
+	var idx = Math.floor(Math.random()*this.words.length);
+	var word = this.words[idx];
+	this.say(word);
+	this.next_idle_handle = setTimeout(this.onIdle.bind(this), 10000);
+}
+
+Character.prototype.onClick = function() {
+}
+
+Character.prototype.hourUpdate = function() {
+	this.words = this.config.scripts.idle.words
+		.filter(function(word) {
+			return word.predicate === true || word.predicate();
+		}).map(function(word) {
+			return word.word;
+		});
+	console.log(this.words);
+}
+
+Character.prototype.onHour = function() {
+	this.hourUpdate();
+}
+
 var character = new Character({
-	path: chrome.extension.getURL("assets/Atago/pannpakapann.jpg")
+	scripts: {
+		// Idle scripts
+		idle: {
+			animation: "idle",
+			words: [
+				// Copied from http://xn--eck5eb7eb.gamerch.com/%E5%9C%92%E7%94%B0%20%E6%B5%B7%E6%9C%AA
+				{
+					word: "一緒にスクールアイドルの高みを目指しましょうね",
+					predicate: true
+				},
+				{
+					word: "過ごしやすい季節でなによりです。",
+					predicate: true
+				},
+				{
+					word: "暑いからってだらしのないのはダメですよ？",
+					predicate: true
+				},
+				{
+					word: "色んなものが美味しい季節ですね。",
+					predicate: function () {return (new Date()).getHours() > 18;}
+				},
+				{
+					word: "穂乃果の家は和菓子屋なのですが...そこのおまんじゅうが絶品なんです。",
+					predicate: function () {return (new Date()).getHours() < 18;}
+				}
+			]
+		},
+		// not used now
+		click: {
+			animation: "idle",
+		},
+		// not used now
+		hour: {
+			animation: "idle",
+		}
+	},
+	animations: {
+		idle: [
+			{
+				path: "Umi/1.png",
+				time: 2000
+			},
+			{
+				path: "Umi/3.png",
+				time: 300
+			},
+			{
+				path: "Umi/2.png",
+				time: 2000
+			},
+			{
+				path: "Umi/3.png",
+				time: 300
+			}
+		]
+	}
 });
