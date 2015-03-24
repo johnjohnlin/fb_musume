3f7b404d98fb6b8eb61f2197fe4cbd427a5f5ff9
wmin0 <wmin0@hotmail.com>
2015-03-24 23:43:20 +0800

    add voice in i18n

--- src/i18n.js
+++ src/i18n.js
@@ -31,13 +31,13 @@ I18n.prototype.loadStringsFullpath = function(path, callback) {
 		callback.call(this);
 		return;
 	}
-	var path = chrome.extension.getURL(path);
+	var true_path = chrome.extension.getURL(path);
 	var xhr = new XMLHttpRequest();
 	xhr.addEventListener('load', function(event) {
-		this.onStringsLoad(event, filename);
+		this.onStringsLoad(event, filename, path);
 		callback.call(this);
 	}.bind(this));
-	xhr.open("GET", path, true);
+	xhr.open("GET", true_path, true);
 	xhr.send();
 }
 
@@ -46,10 +46,19 @@ I18n.prototype.loadStrings = function(filename, callback) {
 	this.loadStringsFullpath(path, callback);
 }
 
-I18n.prototype.onStringsLoad = function(event, filename) {
+I18n.prototype.onStringsLoad = function(event, filename, path) {
 	// TODO: error handling
 	// this.strings[filename] = this.strings[filename] || {};
 	this.strings[filename] = JSON.parse(event.target.responseText);
+	var dir = path.split('/');
+	dir.pop();
+	dir = dir.join('/');
+	Object.keys(this.strings[filename]).forEach(function(key) {
+		var v = this.strings[filename][key].voice;
+		if (v) {
+			this.strings[filename][key].voice = [dir, v].join('/');
+		}
+	}, this);
 }
 
 I18n.prototype.t = function(key, params, filename) {
@@ -66,6 +75,11 @@ I18n.prototype.t = function(key, params, filename) {
 		});
 	}
 	return string;
+}
+
+I18n.prototype.v = function(key, filename) {
+	filename = filename || this.defaultFilename;
+	return this.strings[filename][key]['voice'] || "";
 }
 
 window.I18n = I18n;
